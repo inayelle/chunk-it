545f5c73f967fc58eafd7982a62c8857abe180b3
Vladyslav Kalashnykov-Travin <16034470+inayelle@users.noreply.github.com>
2025-10-02 12:12:07 +0300

    Fix histogram bins

--- src/ChunkIt.Sandbox/Plotting/GenerateDistributionPlotPipe.cs
+++ src/ChunkIt.Sandbox/Plotting/GenerateDistributionPlotPipe.cs
@@ -51,10 +51,18 @@ internal sealed class GenerateDistributionPlotPipe : IPlottingPipe
 
     private static Histogram CreateHistogram(ChunkingReport report)
     {
+        var minimumChunkLength = report.Chunks.Min(chunk => chunk.Length);
+        var maximumChunkLength = report.Chunks.Max(chunk => chunk.Length);
+
+        if (minimumChunkLength == maximumChunkLength)
+        {
+            minimumChunkLength = 0;
+        }
+
         var histogram = Histogram.WithBinSize(
             binSize: 256,
-            firstBin: report.Chunks.Min(chunk => chunk.Length),
-            lastBin: report.Chunks.Max(chunk => chunk.Length)
+            firstBin: minimumChunkLength,
+            lastBin: maximumChunkLength
         );
 
         var values = report
